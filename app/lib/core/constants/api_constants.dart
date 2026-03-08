c0205ed865f2c4464176ebc9f54b48f909808718
kangraemin <remin1994@gmail.com>
2026-03-08 14:15:20 +0900

    fix: 로컬 서버 연결을 위한 iOS ATS 허용 및 서버 URL 변경

--- app/lib/core/constants/api_constants.dart
+++ app/lib/core/constants/api_constants.dart
@@ -1,7 +1,7 @@
 class ApiConstants {
   ApiConstants._();
 
-  static const defaultServerUrl = 'http://158.179.166.232:8000';
+  static const defaultServerUrl = 'http://192.168.0.27:8000';
   static const transcriptEndpoint = '/api/v1/transcript';
   static const summarizeEndpoint = '/api/v1/summarize';
   static const chatEndpoint = '/api/v1/chat';
